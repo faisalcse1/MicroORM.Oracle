05e1b1163eef5987373e5faf7cdd25d413b26ff4
Md Faisal <faisalcse1@gmail.com>
2024-10-31 12:17:41 +0600

    DatabaseContextServiceCollectionExtensions

---
+++ DatabaseContextServiceCollectionExtensions.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using System;
+
+namespace MicroORM.Oracle
+{
+    public static class DatabaseContextServiceCollectionExtensions
+    {
+        public static IServiceCollection AddDatabaseContext(this IServiceCollection services, Action<DatabaseContextOptions> setupOptions)
+        {
+            if (services == null)
+            {
+                throw new ArgumentNullException(nameof(services));
+            }
+
+            if (setupOptions == null)
+            {
+                throw new ArgumentNullException(nameof(setupOptions));
+            }
+            services.AddOptions();
+            services.Configure(setupOptions);
+            services.TryAddSingleton<DatabaseContext>();
+            return services;
+        }
+    }
+}
